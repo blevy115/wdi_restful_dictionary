a8094adbc9b6c0de1f12b8340f83452d23550ca0
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-21 16:10:50 -0500

    created edit and update methods

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -11,4 +11,17 @@ class EntriesController < ApplicationController
   def new
     render :new
   end
+
+  def create
+    redirect_to entries_url
+  end
+
+  def edit
+    render :edit
+  end
+
+  def update
+    redirect_to entry_url(params[:id])
+  end
+
 end
