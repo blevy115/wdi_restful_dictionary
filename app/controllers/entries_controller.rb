503773bbf0839e112afeb730477869a4ff25361f
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-11-22 17:57:39 -0500

    added new and create view

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,19 +1,28 @@
 class EntriesController < ApplicationController
 
   def index
-    # render :index
+    @entry = Entry.all
   end
 
   def show
-    # @entry = Entry.find_by(params[:id])
+    @entry = Entry.find_by(id: params[:id])
   end
 
   def new
-    # render :new
+    @entry = Entry.new
   end
 
   def create
-    redirect_to entries_url
+    @entry = Entry.new
+    @entry.word = params[:entry][:word]
+    @entry.definition = params[:entry][:definition]
+    @entry.language = params[:entry][:language]
+
+    if @entry.save
+      redirect_to entries_url
+    else
+      redirect_to new_entry_url
+    end
   end
 
   def edit
